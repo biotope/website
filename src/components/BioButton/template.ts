a15f38c90e144ea456b03e68994fd6949858a2f2
Ivaylo Stoyanov <ivaylo.stoyanov@virtual-identity.com>
2019-09-04 11:00:07 +0200

    added more colors to _settings for button gradients, changed colors to variables, added a hover effect, changed buttons to a tags

--- src/components/BioButton/template.ts
+++ src/components/BioButton/template.ts
@@ -1,11 +1,11 @@
-import * as styles from './styles.scss';
+import * as styles from './index.scss';
 
 import { BioButtonProps, BioButtonState, BioButtonMethods } from './defines';
 
 
 export default (render: Function, data: BioButtonProps & BioButtonState & BioButtonMethods , createStyle: Function) => {
-    // return render`
-    //     ${createStyle(styles)}
-    //     <div>Fill me</div>
-    // `;
+    return render`
+        ${createStyle(styles)}
+        <div>Fill me</div>
+    `;
 }
