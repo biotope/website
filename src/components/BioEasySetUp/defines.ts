c1b773f8290573cceedede60ef80ca704910fd69
Ivaylo Stoyanov <ivaylo.stoyanov@virtual-identity.com>
2019-09-17 15:02:07 +0200

    created soEasySection

---
+++ src/components/BioEasySetUp/defines.ts
@@ -0,0 +1,32 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BioEasySetUp
+ **/
+
+/**
+ * Props
+ */
+ interface BioEasySetUpProps {
+	imgsrc: string;
+	position?: string;
+ }
+
+/**
+ * State
+ */
+ interface BioEasySetUpState {
+
+ }
+
+/**
+ * Methods
+ */
+ interface BioEasySetUpMethods {
+
+ }
+
+
+export {
+    BioEasySetUpProps,
+	BioEasySetUpState,
+	BioEasySetUpMethods
+}
