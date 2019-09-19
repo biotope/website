de3abc00fedb66da809d8a292845f3ef4fc8aa81
Lukas Müller <lukas.mueller@virtual-identity.com>
2019-09-19 15:52:12 +0200

    42 whos behind biotope (#49)
    * feat(biomodal): implement first steps of bioModal
    
    re #42
    
    * feat(biomodal): fully implement modal
    
    * feat(whosbehind): adjustments to modal, implement bioCollaboratorList
    
    re #42
    
    * chore: merge develop branch
    
    clean out merge conflicts
    
    * improved css like Ivaylo suggested

---
+++ src/components/BioModal/defines.ts
@@ -0,0 +1,31 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BioModal
+ **/
+
+/**
+ * Props
+ */
+ interface BioModalProps {
+	 openonpageload: Boolean;
+}
+
+/**
+ * State
+ */
+interface BioModalState {
+	open: Boolean;
+ }
+
+/**
+ * Methods
+ */
+ interface BioModalMethods {
+
+ }
+
+
+export {
+    BioModalProps,
+	BioModalState,
+	BioModalMethods
+}
