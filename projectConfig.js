e22ef42fb18e2d5f240e11b7c21c8f1385d63cb6
Lukas Müller <lukas.mueller@virtual-identity.com>
2019-09-24 11:37:42 +0200

    feat(compatibility): add IE11 polyfills

--- projectConfig.js
+++ projectConfig.js
@@ -7,7 +7,10 @@ module.exports = {
 		externalResources: {
 			'vi-css-helper': ['helper.css', 'print.css'],
 			'sanitize.css': 'sanitize.css',
-			'handlebars': 'dist/handlebars.runtime.js'
+			'handlebars': 'dist/handlebars.runtime.js',
+			'@webcomponents/webcomponentsjs': [
+				'custom-elements-es5-adapter.js'
+			  ]
 		},
 		tsEntryPoints: [
 			'resources/ts/**/*.ts',
@@ -16,5 +19,16 @@ module.exports = {
 	},
 	webpack: {
 		watchScss: true
-	}
+	},
+	uglify: {
+		ignoreList: [
+		  'src/resources/js/polyfills/object-assign.polyfill.js',
+		  'resources/js/vendor/custom-elements-es5-adapter.js',
+		  'resources/js/polyfills/webcomponents-loader.js',
+		  'resources/js/polyfills/bundles/webcomponents-ce.js',
+		  'resources/js/polyfills/bundles/webcomponents-sd-ce-pf.js',
+		  'resources/js/polyfills/bundles/webcomponents-sd-ce.js',
+		  'resources/js/polyfills/bundles/webcomponents-sd.js'
+		]
+	  }
 };
