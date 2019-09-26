fec6df2f421ca6de56c2849659bce5d268ed82e9
clemensf <clemens.fiedler@posteo.de>
2019-09-26 11:35:09 +0200

    9 tracking (#58)
    * adding tracking
    
    * adjusted naming, meged develop
    
    * improved tracking script
    
    * removed unecessary package @types/google.analytics
    
    * Implement tracking: internal/external links, mailto links and modal links

--- src/resources/ts/tracking.ts
+++ src/resources/ts/tracking.ts
@@ -2,7 +2,14 @@ declare var dataLayer: any[];
 dataLayer = dataLayer || [];
 
 {
-	function trackAnchor(anchorTitle: string) {
+	const internalHostnames = [
+		'localhost',
+		'biotope.sh',
+		'build.biotope.sh',
+		'www.biotope.sh'
+	];
+
+	function trackAnchorLink(anchorTitle: string) {
 		dataLayer.push({
 			'event': 'anchor',
 			'anchor': anchorTitle
@@ -22,16 +29,57 @@ dataLayer = dataLayer || [];
 			'header': slideHeader
 		});
 	}
+	
+	function trackLink(link: HTMLElement) {
+		const linkHref = link.getAttribute('url') ||  link.getAttribute('href');
+		const linkURL = new URL(linkHref);
+		const linkText = link.getAttribute('title') || link.innerText;
+		const linkHostname = linkURL ? linkURL.hostname : '';
+		if (internalHostnames.indexOf(linkHostname) != -1) {
+			trackCallToAction('cta-intern', linkText);
+		} else {
+			trackCallToAction('cta-extern', linkText);
+		}
+	}
 
-	document.querySelectorAll('bio-button[url^="#"]').forEach(button => {
+	function trackMailtoLink(mailtoLink: HTMLElement) {
+		const linkHref = mailtoLink.getAttribute('url') ||  mailtoLink.getAttribute('href');
+		const eMailAddress = linkHref.replace('mailto:', '');
+		dataLayer.push({
+			'event': 'contact',
+			'eMail': eMailAddress
+		});
+	}
+
+	function trackModalLink(modalLink: HTMLElement) {
+		const title = modalLink.getAttribute('title') || modalLink.innerText;
+		dataLayer.push({
+			'event': 'modal',
+			'overlay': title
+		});
+	}
+
+	document.querySelectorAll('a[href^="#"], bio-button[url^="#"]').forEach(button => {
 		button.addEventListener('click', () => {
-			trackAnchor(button.getAttribute('title'));
+			trackAnchorLink(button.getAttribute('title'));
 		});
 	});
 
-	document.querySelectorAll('bio-button[url^="http"]').forEach(button => {
-		button.addEventListener('click', () => {
-			trackCallToAction('cta-extern', button.getAttribute('title'));
+	document.querySelectorAll('a[href^="http"], bio-button[url^="http"]').forEach((link: HTMLElement) => {
+		link.addEventListener('click', () => {
+			trackLink(link);
+		});
+	});
+
+	document.querySelectorAll('a[href^="mailto:"], bio-button[url^="mailto:"]').forEach((link: HTMLElement) => {
+		link.addEventListener('click', () => {
+			trackMailtoLink(link);
+		});
+	});
+
+	document.querySelectorAll('.js-openModal').forEach((link: HTMLElement) => {
+		link.addEventListener('click', () => {
+			trackModalLink(link);
 		});
 	});
 
