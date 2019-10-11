a68751629a365c1e33ad1ac5547771603c2763df
Lukas Müller <lukas.mueller@virtual-identity.com>
2019-10-11 13:18:04 +0200

    feat(biocookiebanner): make buttons white

--- src/components/BioCookieBanner/template.ts
+++ src/components/BioCookieBanner/template.ts
@@ -12,8 +12,8 @@ export default (render: Function, data: BioCookieBannerProps & BioCookieBannerSt
             <div class="wrapper-inner">
                 <p>We use cookies so that we can offer you the best possible website experience. These are cookies which are used solely for anonymous statistical purposes to help us improve the user experience on the website. You are free to decide whether you would like to permit these cookies. Further information can be found in our Privacy Policy.</p>
                 <div class="buttons">
-                    <bio-button onclick="${data.handleAccept}" class="accept" modifier="whiteText blue" title="Accept"></bio-button>
-                    <bio-button onclick="${data.handleDecline}" class="decline" modifier="whiteText blue" title="Decline"></bio-button>
+                    <bio-button onclick="${data.handleAccept}" class="accept" modifier="white" title="Accept"></bio-button>
+                    <bio-button onclick="${data.handleDecline}" class="decline" modifier="white" title="Decline"></bio-button>
                 </div>
             </div>
         </div>
