21c81cde4b81f3dbe078ffefc09d2c2ea8a863cb
Ivaylo Stoyanov <IvailoStoyanov@users.noreply.github.com>
2019-10-17 16:48:46 +0200

    127 privacy policy link in cookie banner (#128)
    * refactor part of cookie banner to accept slotted P text so that we can use a link to PrivacyPolicy
    
    * cleaning
    
    * cleaning

--- src/components/BioCookieBanner/template.ts
+++ src/components/BioCookieBanner/template.ts
@@ -10,12 +10,7 @@ export default (render: Function, data: BioCookieBannerProps & BioCookieBannerSt
         ${createStyle(styles)}
         <div class="${classes}">
             <div class="wrapper-inner">
-                <p>We use cookies so that we can offer you the best possible website experience and see whether our marketing campaigns are effective. 
-                    With Google Analytics (cookies expire in 24 months) we gain insights on how users interact with the website to better understand if 
-                    their needs are met. With the LinkedIn Insight Tag (cookies expire in 24 months) we gain insights whether our marketing efforts are 
-                    having the desired effect. You are free to decline these cookies, but we'd be happy if you let us use your anonymized data. Further 
-                    information can be found in our Privacy Policy
-                </p>
+                <slot></slot>
                 <div class="buttons">
                     <bio-button onclick="${data.handleAccept}" class="accept" modifier="white" title="Accept"></bio-button>
                     <bio-button onclick="${data.handleDecline}" class="decline" modifier="white" title="Decline"></bio-button>
