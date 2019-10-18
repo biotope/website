3051eddc0584fd62ec4a07bb18e38c688efd39fc
Ivaylo Stoyanov <IvailoStoyanov@users.noreply.github.com>
2019-10-18 15:17:24 +0200

    mobile cookie banner adjustments (#136)
    * mobile cookie banner adjustments

--- src/components/BioCookieBanner/template.ts
+++ src/components/BioCookieBanner/template.ts
@@ -12,8 +12,8 @@ export default (render: Function, data: BioCookieBannerProps & BioCookieBannerSt
             <div class="wrapper-inner">
                 <slot></slot>
                 <div class="buttons">
-                    <bio-button onclick="${data.handleAccept}" class="accept" modifier="white" title="Accept"></bio-button>
-                    <bio-button onclick="${data.handleDecline}" class="decline" modifier="white" title="Decline"></bio-button>
+                    <bio-button onclick="${data.handleAccept}" class="accept" modifier="white compact" title="Accept"></bio-button>
+                    <bio-button onclick="${data.handleDecline}" class="decline" modifier="white compact" title="Decline"></bio-button>
                 </div>
             </div>
         </div>
