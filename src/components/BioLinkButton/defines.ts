fc1adb23eb4f7f2b5e4111bf8beaab99e2164198
Ivaylo Stoyanov <ivaylo.stoyanov@virtual-identity.com>
2019-10-21 16:55:30 +0200

    Changing the contact buttons to links

---
+++ src/components/BioLinkButton/defines.ts
@@ -0,0 +1,33 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BioLinkButton
+ **/
+
+/**
+ * Props
+ */
+ interface BioLinkButtonProps {
+	title: string,
+	modifier?: string,
+	url?: string
+ }
+
+/**
+ * State
+ */
+ interface BioLinkButtonState {
+
+ }
+
+/**
+ * Methods
+ */
+ interface BioLinkButtonMethods {
+
+ }
+
+
+export {
+    BioLinkButtonProps,
+	BioLinkButtonState,
+	BioLinkButtonMethods
+}
--- src/resources/ts/tracking.ts
+++ src/resources/ts/tracking.ts
@@ -108,7 +108,7 @@ initGoogleAnalytics();
 	}
 
 	function trackLink(link: HTMLElement) {
-		const linkHref = link.getAttribute("url") || link.getAttribute("href");
+		const linkHref = link.getAttribute("href");
 		const linkURL = new URL(linkHref);
 		const linkHostname = linkURL ? linkURL.hostname : "";
 		if (internalHostnames.indexOf(linkHostname) != -1) {
@@ -119,7 +119,7 @@ initGoogleAnalytics();
 	}
 
 	function trackMailtoLink(mailtoLink: HTMLElement) {
-		const linkHref = mailtoLink.getAttribute("url") || mailtoLink.getAttribute("href");
+		const linkHref = mailtoLink.getAttribute("href");
 		const eMailAddress = linkHref.replace("mailto:", "");
 		dataLayer.push({
 			event: "contact",
